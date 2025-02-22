2397bf26c858a058eeee7b767ecf4d0dfa75d31f
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-22 10:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let body=document.querySelector("body");
 let div=document.createElement("div");
 div.setAttribute("id","browser-info");
 
-div.innerHTML=`<p>You are using ${navigator.userAgent} ${navigator.appName} ${navigator.Version}.</p>`
+div.innerHTML=`<p>You are using ${navigator.userAgent} ${navigator.appName} version ${navigator.Version}.</p>`
 body.appendChild(div);
